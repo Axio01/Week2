1ae16838c5fd17fd9abe85799f5bded6bd46de42
Live session user <xiong.adam019@gmail.com>
2018-02-21 23:17:33 +0000

    first

---
+++ Hello.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Hello World!"
+
+read -p "Please enter your name!: " name
+
+echo -e "Hello $name!" 
+
+read -p "Could you please pick a number?: " x 
+
+read -p "Why did you pick this number?: " x
+
+ehco -e "No more questions $name!"
+
+echo "Thank You!"
+
+echo "Good Bye $name!"
+
+sleep 2
+
+clear 
+
+#END
